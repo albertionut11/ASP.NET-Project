66b885c8f0b45d202e46854d77e78aa298ad1e13
Albert_Balauta <albertionutro@gmail.com>
2022-12-21 14:29:48 +0200

    partially solved domain and articles CRUD

--- DAW_Project/Data/Migrations/20221221121250_Upadte1.Designer.cs
+++ DAW_Project/Data/Migrations/20221221121250_Upadte1.Designer.cs
@@ -4,6 +4,7 @@ using DAW_Project.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DAW_Project.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221221121250_Upadte1")]
+    partial class Upadte1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
