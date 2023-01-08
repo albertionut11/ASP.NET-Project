429d8b45ae45a646b50e34aa8805a0e66a833155
Tudor <tudor_haulica@yahoo.com>
2023-01-08 23:33:06 +0200

    adaugare_editare

--- DAW_Project/Controllers/ArticlesController.cs
+++ DAW_Project/Controllers/ArticlesController.cs
@@ -1,10 +1,14 @@
 ﻿using DAW_Project.Data;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using DAW_Project.Models;
-using System.Data;
+using DAW_Project.Controllers;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Linq;
 
 namespace DAW_Project.Controllers
 {
@@ -79,42 +83,53 @@ namespace DAW_Project.Controllers
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            catch (DataException)
+            catch (Exception e)
             {
-                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+                return View();
             }
+           
             return View(article);
             
         }
-
-
-        [Authorize(Roles = "Editor,Admin")]
-        public IActionResult Edit(int id)
+        public IEnumerable<SelectListItem> GetAllDomains()
         {
-            Article article = db.Articles.Find(id);
-            ViewBag.Article = article;
-            return View();
+            // generam o lista de tipul SelectListItem fara elemente
+            var selectList = new List<SelectListItem>();
+            
+            // extragem toate categoriile din baza de date
+            var categories = from dom in db.Domains
+                             select dom;
+
+            // iteram prin categorii
+            foreach (var category in categories)
+            {
+                // adaugam in lista elementele necesare pentru dropdown
+                // id-ul categoriei si denumirea acesteia
+                selectList.Add(new SelectListItem
+                {
+                    Value = category.Id.ToString(),
+                    Text = category.Domain_name.ToString()
+                });
+            }
+            /* Sau se poate implementa astfel: 
+             * 
+            foreach (var category in categories)
+            {
+                var listItem = new SelectListItem();
+                listItem.Value = category.Id.ToString();
+                listItem.Text = category.CategoryName.ToString();
+
+                selectList.Add(listItem);
+             }*/
+
+
+            // returnam lista de categorii
+            return selectList;
         }
+
         [HttpPost]
         [Authorize(Roles = "Editor,Admin")]
-        public ActionResult Edit(int id, Article requestArticle)
-        {
-            Article article = db.Articles.Find(id);
-            try
-            {
-                article.Title = requestArticle.Title;
-                article.Editor_Name = requestArticle.Editor_Name;
-                article.Content = requestArticle.Content;
-                db.SaveChanges();
-
-                return RedirectToAction("Index");
-            }
-            catch (Exception)
-            {
-                return RedirectToAction("Edit", article.Article_Id);
-            }
-        }
-
+       
         public IActionResult Delete(int id)
         {
             Article article = db.Articles.Find(id);
@@ -122,6 +137,73 @@ namespace DAW_Project.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+
+        [Authorize(Roles = "Editor,Admin")]
+        public IActionResult Edit(int id)
+        {
+
+            Article article = db.Articles.Include("Domain")
+                                        .Where(art => art.Article_Id == id)
+                                        .First();
+
+            article.Dom = GetAllDomains();
+
+            if (article.UserID == _userManager.GetUserId(User) || User.IsInRole("Admin"))
+            {
+                return View(article);
+            }
+
+            else
+            {
+                TempData["message"] = "Nu aveti dreptul sa faceti modificari asupra unui articol care nu va apartine";
+                return RedirectToAction("Index");
+            }
+
+        }
+
+        // Se adauga articolul modificat in baza de date
+        [HttpPost]
+        [Authorize(Roles = "Editor,Admin")]
+        public IActionResult Edit(int id, Article requestArticle)
+        {
+            
+
+            Article article = db.Articles.Find(id);
+
+            var errors = ModelState.Where(x => x.Value.Errors.Any())
+                .Select(x => new { x.Key, x.Value.Errors });
+
+            if (ModelState.IsValid)
+            {
+                if (article.UserID == _userManager.GetUserId(User) || User.IsInRole("Admin"))
+                {
+                    article.Title = requestArticle.Title;
+
+                  
+
+                    article.Content = requestArticle.Content;
+
+                    article.Domain_id = requestArticle.Domain_id;
+                    TempData["message"] = "Articolul a fost modificat";
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["message"] = "Nu aveti dreptul sa faceti modificari asupra unui articol care nu va apartine";
+                    return RedirectToAction("Index");
+                }
+            }
+            else
+            {
+                requestArticle.Dom = GetAllDomains();
+                return View(requestArticle);
+            }
+        }
+
+
+
 
 
     }
--- DAW_Project/Models/Article.cs
+++ DAW_Project/Models/Article.cs
@@ -11,7 +11,7 @@ namespace DAW_Project.Models
         public int Article_Id { get; set; }
         [Required(ErrorMessage = "Titlul este obligatoriu!")]
         public string Title { get; set; }
-        public string Editor_Name { get; set; }
+        public string? Editor_Name { get; set; }
         public DateTime Post_Date { get; set; }
         [Required(ErrorMessage = "Continutul articolului este obligatoriu!")]
         public string Content { get; set; }
@@ -19,9 +19,9 @@ namespace DAW_Project.Models
         public virtual ApplicationUser? User { get; set; }
         [Required(ErrorMessage = "Domeniul articolului este obligatoriu!")]
         public int? Domain_id { get; set; }
-        public virtual Domain Domain { get; set; }
+        public virtual Domain? Domain { get; set; }
 
-        public virtual ICollection<Modification> Modifications { get; set; }
+        public virtual ICollection<Modification>? Modifications { get; set; }
 
         [NotMapped]
         public IEnumerable<SelectListItem>? Dom { get; set; }
